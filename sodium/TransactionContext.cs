f27b1f72815ceb97be58801abcdd5c28812bd953
Jerome Terry <jerome.l.terry@gmail.com>
2014-02-25 14:50:12 -0330

    Making TransactionContext a static class
    Instead of having to create an instance of TransactionContext,, the Run
    method does all the work. This is pretty much the original
    Transaction.Run method, just moved into a separate class.

--- sodium/TransactionContext.cs
+++ sodium/TransactionContext.cs
@@ -3,44 +3,14 @@
     using System;
 
     /// <summary>
-    /// TransactionContext is used by the Transaction.Run to ensure that only a
-    /// single Transaction instance is created.
+    /// TransactionContext is used to ensure that only a single Transaction instance is created.
     /// </summary>
-    /// <remarks>Transaction.Run may be invoked recursively indirectly. Hence the
-    /// need to ensure that the root Transaction.Run is the only one to create
+    /// <remarks>TransactionContext.Run may be invoked recursively indirectly. Hence the
+    /// need to ensure that the root TransactionContext.Run is the only one to create
     /// a new Transaction object.</remarks>
-    internal sealed class TransactionContext : IDisposable
+    internal static class TransactionContext
     {
-        /// <summary>
-        /// Transaction for use by the static method Transaction.Run
-        /// </summary>
-        /// <remarks>
-        /// If the Open method initializes current, then the Close method will 
-        /// dispose of the created transaction.
-        /// 
-        /// If current is already initialized when the Open method is invoked,
-        /// current is unmodified and the Close method will not dispose current.
-        /// 
-        /// current is only disposed by the TransactionContext that created it.
-        /// </remarks>
         private static Transaction current;
-
-        private Transaction previous;
-        private Transaction created;
-        private bool restored;
-        private bool disposed;
-
-        private TransactionContext()
-        {
-        }
-
-        public Transaction Transaction
-        {
-            get
-            {
-                return current;
-            }
-        }
 
         /// <summary>
         /// Run the specified function inside a single transaction
@@ -57,54 +27,31 @@
         {
             lock (Constants.TransactionLock)
             {
-                using (var context = new TransactionContext())
+                // If we are already inside a transaction (which must be on the same
+                // thread otherwise we wouldn't have acquired transactionLock), then
+                // keep using that same transaction.
+                var previous = current;
+                Transaction created = null;
+
+                if (current == null)
                 {
-                    context.Open();
-                    return f(context.Transaction);
+                    created = new Transaction();
+                    current = created;
+                }
+
+                try
+                {
+                    return f(current);
+                }
+                finally 
+                {
+                    if (created != null)
+                    {
+                        created.Dispose();
+                        current = previous;
+                    }
                 }
             }
-        }
-
-        public void Open()
-        {
-            // If we are already inside a transaction (which must be on the same
-            // thread otherwise we wouldn't have acquired transactionLock), then
-            // keep using that same transaction.
-            previous = current;
-
-            if (current == null)
-            {
-                created = new Transaction();
-                current = created;
-            }
-        }
-
-        public void Close()
-        {
-            if (created != null)
-            {
-                created.Dispose();
-                created = null;
-            }
-        
-            if (!restored)
-            {
-                current = previous;
-                restored = true;
-            }
-            
-            previous = null;
-        }
-
-        public void Dispose()
-        {
-            if (disposed)
-            {
-                return;
-            }
-
-            Close();
-            disposed = true;
         }
     }
 }
