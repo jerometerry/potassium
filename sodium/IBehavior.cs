9ee316eb7b3468155288937b5524ca76f8280adf
Jerome Terry <jerome.l.terry@gmail.com>
2014-03-17 00:22:26 -0230

    Tyding up interfces

--- sodium/IBehavior.cs
+++ sodium/IBehavior.cs
@@ -3,16 +3,25 @@ namespace Sodium
 {
     public interface IBehavior<T> : IObservable<T>
     {
-        IBehavior<TB> Apply<TB>(IBehavior<Func<T, TB>> bf);
-        IBehavior<TB> Collect<TB, TS>(TS initState, Func<T, TS, Tuple<TB, TS>> snapshot);
-        T GetNewValue();
-        IBehavior<TD> Lift<TB, TC, TD>(Func<T, TB, TC, TD> lift, IBehavior<TB> b, IBehavior<TC> c);
-        IBehavior<TC> Lift<TB, TC>(Func<T, TB, TC> lift, IBehavior<TB> behavior);
-        IBehavior<TB> Map<TB>(Func<T, TB> map);
-        ISubscription<T> SubscribeWithFire(Action<T> callback);
-        ISubscription<T> SubscribeWithFire(ISodiumCallback<T> callback, Rank rank);
         T Value { get; }
 
         IEvent<T> Values(Transaction transaction);
+
+        T GetNewValue();
+
+        IBehavior<TB> Apply<TB>(IBehavior<Func<T, TB>> bf);
+        
+        IBehavior<TB> Collect<TB, TS>(TS initState, Func<T, TS, Tuple<TB, TS>> snapshot);
+        
+        IBehavior<TD> Lift<TB, TC, TD>(Func<T, TB, TC, TD> lift, IBehavior<TB> b, IBehavior<TC> c);
+        
+        IBehavior<TC> Lift<TB, TC>(Func<T, TB, TC> lift, IBehavior<TB> behavior);
+        
+        IBehavior<TB> Map<TB>(Func<T, TB> map);
+        
+        ISubscription<T> SubscribeWithFire(Action<T> callback);
+        
+        ISubscription<T> SubscribeWithFire(ISodiumCallback<T> callback, Rank rank);
+        
     }
 }
--- sodium/IEvent.cs
+++ sodium/IEvent.cs
@@ -4,16 +4,25 @@ namespace Sodium
     public interface IEvent<T> : IObservable<T>, ISnapshot<T>, IHoldable<T>
     {
         IBehavior<TS> Accum<TS>(TS initState, Func<T, TS, TS> snapshot);
-        bool CancelSubscription(ISubscription<T> subscription);
+
         IEvent<T> Coalesce(Func<T, T, T> coalesce);
+        
         IEvent<TB> Collect<TB, TS>(TS initState, Func<T, TS, Tuple<TB, TS>> snapshot);
+        
         IEvent<T> Delay();
+        
         IEvent<T> Filter(Func<T, bool> predicate);
+        
         IEvent<T> FilterNotNull();
+        
         IEvent<T> Gate(IBehavior<bool> predicate);
+        
         IEvent<TB> Map<TB>(Func<T, TB> map);
+        
         IEvent<T> Merge(IEvent<T> source);
+        
         IEvent<T> Merge(IEvent<T> source, Func<T, T, T> coalesce);
+        
         IEvent<T> Once();
     }
 }
