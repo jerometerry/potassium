eff9251270682842742c2b066164a64071eafb4c
Jerome Terry <jerome.l.terry@gmail.com>
2014-03-19 18:40:43 -0230

    Cleaning up using statements

--- sodium/EventExtensions.cs
+++ sodium/EventExtensions.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Sodium
+﻿namespace Sodium
 {
+    using System;
+
     /// <summary>
     /// Reactive extensions for Events
     /// </summary>
