1bec9452a2aaa33144f6f990630305eedfa46666
Jerome Terry <jerome.l.terry@gmail.com>
2014-03-20 13:38:54 -0230

    Added HttpBehavior and tests
    HttpBehavior is a Behavior that fetches it's values via a Web Request.

---
+++ sodium/HttpBehavior.cs
@@ -0,0 +1,54 @@
+﻿namespace Sodium
+{
+    using System.Net;
+
+    /// <summary>
+    /// WebRequestBehavior is a Behavior that downloads a URL as a string
+    /// </summary>
+    public class HttpBehavior : Behavior<string>
+    {
+        private Behavior<string> urlBehavior;
+        
+        /// <summary>
+        /// Constructs a new WebRequestBehavior to fetch from a constant URL
+        /// </summary>
+        /// <param name="url">The URL to load the content from</param>
+        public HttpBehavior(string url)
+        {
+            this.urlBehavior = new ConstantBehavior<string>(url);
+            this.Register(this.urlBehavior);
+        }
+
+        /// <summary>
+        /// Constructs a new WebRequestBehavior to fetch from a dynamic URL
+        /// </summary>
+        /// <param name="urlBehavior">Behavior containing the URL to load</param>
+        public HttpBehavior(Behavior<string> urlBehavior)
+        {
+            this.urlBehavior = urlBehavior;
+        }
+
+        public override string Value
+        {
+            get
+            {
+                var url = urlBehavior.Value;
+                using (var client = new WebClient())
+                {
+                    string result = client.DownloadString(url);
+                    return result;
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.urlBehavior = null;
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ tests/UnitTests/HttpBehaviorTester.cs
@@ -0,0 +1,26 @@
+﻿namespace Sodium.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class HttpBehaviorTester
+    {
+        [Test]
+        public void LoadGoogle()
+        {
+            var behavior = new HttpBehavior("http://google.com");
+            var source = behavior.Value;
+            Console.WriteLine("Google.com Source: {0}", source);
+            Assert.IsNotNullOrEmpty(source);
+        }
+        [Test]
+        public void LoadJsonTestIp()
+        {
+            var behavior = new HttpBehavior("http://ip.jsontest.com/");
+            var json = behavior.Value;
+            Console.WriteLine("JSON: {0}", json);
+            Assert.IsNotNullOrEmpty(json);
+        }
+    }
+}
