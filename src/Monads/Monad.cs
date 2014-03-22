665de588b3b132a0c8ed9b0743517606027db10b
Jerome Terry <jerome.l.terry@gmail.com>
2014-03-22 11:22:40 -0230

    Linking Mike Hadlows blog

--- src/Monads/Monad.cs
+++ src/Monads/Monad.cs
@@ -6,6 +6,7 @@
     /// A Monad lazily evaluates a value, and has monadic operations that allow Monads to be chained together
     /// </summary>
     /// <typeparam name="T"></typeparam>
+    /// <remarks>http://mikehadlow.blogspot.co.uk/2011/01/monads-in-c1-introduction.html</remarks>
     public abstract class Monad<T> : Disposable, IProvider<T>
     {
         /// <summary>
