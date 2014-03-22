f387f3575e721e823c2b2548516651683ab39803
Jerome Terry <jerome.l.terry@gmail.com>
2014-03-22 11:20:20 -0230

    Adding the state monad
    http://taumuon-jabuka.blogspot.ca/2012/06/state-monad-in-c-and-f.html?m=1

--- src/Extensions/MonadExtensions.cs
+++ src/Extensions/MonadExtensions.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using JT.Rx.Net.Core;
-    using JT.Rx.Net.Internal;
     using JT.Rx.Net.Monads;
 
     /// <summary>
@@ -33,6 +32,25 @@
         public static Monad<TD> Lift<TA, TB, TC, TD>(Func<TA, TB, TC, TD> lift, IProvider<TA> a, IProvider<TB> b, IProvider<TC> c)
         {
             return new TernaryMonad<TA, TB, TC, TD>(lift, a, b, c);
+        }
+
+        public static State<TS, TB> Bind<TS, TA, TB>(this State<TS, TA> a, Func<TA, State<TS, TB>> func)
+        {
+            return new State<TS, TB>(x =>
+            {
+                var stateContent = a.Computation(x);
+                return func(stateContent.Item2).Computation(stateContent.Item1);
+            });
+        }
+
+        public static State<TS, TA> ToState<TS, TA>(this TA value)
+        {
+            return new State<TS, TA>(state => Tuple.Create(state, value));
+        }
+
+        public static State<TS, TC> SelectManay<TS, TA, TB, TC>(this State<TS, TA> a, Func<TA, State<TS, TB>> func, Func<TA, TB, TC> select)
+        {
+            return a.Bind(aVal => func(aVal).Bind(bVal => select(aVal, bVal).ToState<TS, TC>()));
         }
     }
 }
---
+++ src/Monads/State.cs
@@ -0,0 +1,34 @@
+﻿namespace JT.Rx.Net.Monads
+{
+    using System;
+    using JT.Rx.Net.Core;
+
+    /// <summary>
+    /// State Monad
+    /// </summary>
+    /// <typeparam name="TS"></typeparam>
+    /// <typeparam name="TA"></typeparam>
+    /// <remarks>From http://taumuon-jabuka.blogspot.ca/2012/06/state-monad-in-c-and-f.html?m=1</remarks>
+    public class State<TS, TA>
+    {
+        public State(Func<TS, Tuple<TS, TA>> computation)
+        {
+            this.Computation = computation;
+        }
+
+        public Func<TS, Tuple<TS, TA>> Computation { get; private set; }
+    }
+
+    public static class State
+    {
+        public static State<TS,TS> Get<TS>()
+        {
+            return new State<TS, TS>(sx => Tuple.Create(sx, sx));
+        }
+
+        public static State<TS, Unit> Set<TS>(TS state)
+        {
+            return new State<TS, Unit>(sx => new Tuple<TS, Unit>(state, Unit.Nothing));
+        }
+    }
+}
